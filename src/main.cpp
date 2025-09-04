faaf5309ff08658b45cd0d55f3eeb55039623e56
findeevy <findeevy@gmail.com>
2025-09-04 16:35:46 -0500

    added surface creation + set up, compiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,162 @@
 #define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <vulkan/vk_platform.h>
 #include <vulkan/vulkan_raii.hpp>
-#include <GLFW/glfw3.h>
 
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
+const uint32_t WIDTH = 1280;
+const uint32_t HEIGHT = 720;
+
+const std::vector validationLayers = {"VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApplication {
 public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
-  void initVulkan() {}
+  vk::raii::Context context;
+  // Our global Vulkan instance.
+  vk::raii::Instance instance = nullptr;
 
-  void mainLoop() {}
+  vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
+  vk::raii::SurfaceKHR surface = nullptr;
 
-  void cleanup() {}
+  GLFWwindow *window = nullptr;
+
+  void createSurface() {
+    VkSurfaceKHR _surface;
+    if (glfwCreateWindowSurface(*instance, window, nullptr, &_surface) != 0) {
+      throw std::runtime_error("Failed to create window surface!");
+    }
+    surface = vk::raii::SurfaceKHR(instance, _surface);
+  }
+  void setupDebugMessenger() {
+    if (!enableValidationLayers) {
+      return;
+    }
+
+    vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(
+        vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+        vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+        vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+    vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(
+        vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+        vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+        vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+    vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfoEXT{
+        {}, severityFlags, messageTypeFlags, &debugCallback};
+    instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
+  }
+
+  static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(
+      vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
+      vk::DebugUtilsMessageTypeFlagsEXT type,
+      const vk::DebugUtilsMessengerCallbackDataEXT *pCallbackData, void *) {
+    if (severity == vk::DebugUtilsMessageSeverityFlagBitsEXT::eError ||
+        severity == vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning) {
+      std::cerr << "validation layer: type " << to_string(type)
+                << " msg: " << pCallbackData->pMessage << std::endl;
+    }
+
+    return vk::False;
+  }
+  // Retrieves a list of extensions that are required (if in debug mode).
+  std::vector<const char *> getRequiredExtensions() {
+    uint32_t glfwExtensionCount = 0;
+    auto glfwExtensions =
+        glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+    if (enableValidationLayers) {
+      extensions.push_back(vk::EXTDebugUtilsExtensionName);
+    }
+
+    extensions.push_back(vk::KHRGetPhysicalDeviceProperties2ExtensionName);
+
+    return extensions;
+  }
+  void createInstance() {
+    vk::ApplicationInfo appInfo("AcceleRender", VK_MAKE_VERSION(1, 0, 0),
+                                "No Engine", VK_MAKE_VERSION(1, 0, 0),
+                                VK_API_VERSION_1_0);
+
+    std::vector<char const *> requiredLayers;
+    if (enableValidationLayers) {
+      requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+    }
+
+    auto layerProperties = context.enumerateInstanceLayerProperties();
+    for (auto const &requiredLayer : requiredLayers) {
+      if (std::ranges::none_of(
+              layerProperties, [requiredLayer](auto const &layerProperty) {
+                return strcmp(layerProperty.layerName, requiredLayer) == 0;
+              })) {
+        throw std::runtime_error("Required layer not supported: " +
+                                 std::string(requiredLayer));
+      }
+    }
+
+    auto requiredExtensions = getRequiredExtensions();
+
+    auto extensionProperties = context.enumerateInstanceExtensionProperties();
+    for (auto const &requiredExtension : requiredExtensions) {
+      if (std::ranges::none_of(
+              extensionProperties,
+              [requiredExtension](auto const &extensionProperty) {
+                return strcmp(extensionProperty.extensionName,
+                              requiredExtension) == 0;
+              })) {
+        throw std::runtime_error("Required extension not supported: " +
+                                 std::string(requiredExtension));
+      }
+    }
+
+    vk::InstanceCreateInfo createInfo{
+        {},
+        &appInfo,
+        static_cast<uint32_t>(requiredLayers.size()),
+        requiredLayers.data(),
+        static_cast<uint32_t>(requiredExtensions.size()),
+        requiredExtensions.data()};
+    instance = vk::raii::Instance(context, createInfo);
+  }
+
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+  void initVulkan() {
+    createInstance();
+    setupDebugMessenger();
+    createSurface();
+  }
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
 };
 
 int main() {
