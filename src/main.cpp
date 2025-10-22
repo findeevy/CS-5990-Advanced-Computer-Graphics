cef67e240e165e2990c993e2eebb17e71d8db500
ejnewton <eric-newton@uiowa.edu>
2025-10-22 12:03:17 -0500

    Removed readFile from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -645,21 +645,6 @@ private:
     app->framebufferResized = true;
   }
 
-  /*std::vector<char> readFile(const std::string &filename) {
-    std::ifstream file(filename, std::ios::ate | std::ios::binary);
-    std::cout << filename << std::endl;
-    if (!file.is_open()) {
-      throw std::runtime_error("Failed to open file!");
-    }
-
-    size_t fileSize = (size_t)file.tellg();
-    std::vector<char> buffer(fileSize);
-    file.seekg(0);
-    file.read(buffer.data(), fileSize);
-    file.close();
-    return buffer;
-  }*/
-
   void createCommandBuffers() {
     commandBuffers.clear();
     vk::CommandBufferAllocateInfo allocInfo;
